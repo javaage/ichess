e10ad7c095aae03148f8e12d6763444648bdd71e
javaage <hb_java@sina.com>
2018-10-18 21:42:16 +0800

    error

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ require 'common.php';
 
 function exception_handler( Throwable $e){
     echo 'catch Error:'.$e->getCode().':'.$e->getMessage().'<br/>';
+    error_log("Oracle database not available!", 0);
 }
 
 set_exception_handler('exception_handler');
