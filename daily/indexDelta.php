e2f08800087d3ff0e6ed2e897d88d242be1d62c9
javaage <hb_java@sina.com>
2018-10-26 08:40:06 +0800

    fix function bug

--- daily/indexDelta.php
+++ daily/indexDelta.php
@@ -2,63 +2,12 @@
 require '../header.php';
 require '../common.php';
 
-$queryFormat = 'http://47.94.203.104:8001/indexDelta/%s';
-
 foreach ($indexList as $indexCode){
     $code = $indexCode[0];
     echo $code;
-    $queryUrl = sprintf($queryFormat,$code);
-    $html = file_get_contents($queryUrl);
-    $csv = json_decode($html);
-    
-    for ($i = count($csv) - 1; $i > 0; $i--) {
-        $f = $csv[$i];
-        $l = $csv[$i - 1];
-        $w = new Wave();
-        $w -> id = uniqid();
-        $w -> level = 5;
-        $w -> asc = ($l[2] >= $f[2] ? 1 : 0);
-        
-        $n = $csv[$i - 2];
-        while ((($n[2] >= $l[2]) == $w -> asc) && $i > 1) {
-            $i--;
-            $l = $csv[$i - 1];
-            $n = $csv[$i - 2];
-        }
-        
-        $w -> high = max($f[2], $l[2]);
-        $w -> low = min($f[2], $l[2]);
-        $w -> beginTime = strtotime(formatDate($f[0]) . " 15:00:00");
-        $w -> endTime = strtotime(formatDate($l[0]) . " 15:00:00");
-        
-        saveWave($gw, $w);
-        
-    }
-    
-    $nw = $gw;
-    while(count($nw->childWave)>0){
-        $nw = $nw->childWave[count($nw->childWave)-1];
-    }
-    
-    while($nw->level > 2){
-        $child = json_decode(json_encode($nw));
-        $child->id = uniqid();
-        $child->pid = $nw->id;
-        $child->level = $nw->level - 1;
-        $nw->childWave[] = $child;
-        $nw = $child;
-    }
-    
-    if(empty($csv)){
-        return false;
-    }else{
-        $arrow = getArrow($gw);
-        $ac = countIndexArrow($gw);
-        $format="INSERT INTO wavedaily (code,dt,gw,arrow,ac) VALUES('%s','%s','%s','%s',%d) ON DUPLICATE KEY UPDATE dt='%s',gw='%s',arrow='%s',ac=%d";
-        $strQuery = sprintf($format,$code,formatDate($csv[0][0]),json_encode($gw),$arrow,$ac,formatDate($csv[0][0]),json_encode($gw),$arrow,$ac);
-        $mysql -> query($strQuery);
-    }
+    recordWave($code);
 }
+
 function saveHistory(&$node) {
     global $mysql;
     if (empty($node))
@@ -92,5 +41,61 @@ function countIndexArrow($gw)
             $gw = $gw->childWave[$cn - 1];
         }
         return $r;
+    }
+}
+
+function recordWave($code){
+    global $mysql, $kv;
+    
+    $queryFormat = 'http://47.94.203.104:8001/indexDelta/%s';
+    $queryUrl = sprintf($queryFormat,$code);
+    $html = file_get_contents($queryUrl);
+    $csv = json_decode($html);
+    
+    for ($i = count($csv) - 1; $i > 0; $i--) {
+        $f = $csv[$i];
+        $l = $csv[$i - 1];
+        $w = new Wave();
+        $w -> id = uniqid();
+        $w -> level = 5;
+        $w -> asc = ($l[2] >= $f[2] ? 1 : 0);
+        
+        $n = $csv[$i - 2];
+        while ((($n[2] >= $l[2]) == $w -> asc) && $i > 1) {
+            $i--;
+            $l = $csv[$i - 1];
+            $n = $csv[$i - 2];
+        }
+        
+        $w -> high = max($f[2], $l[2]);
+        $w -> low = min($f[2], $l[2]);
+        $w -> beginTime = strtotime(formatDate($f[0]) . " 15:00:00");
+        $w -> endTime = strtotime(formatDate($l[0]) . " 15:00:00");
+        
+        saveWave($gw, $w);
+    }
+    
+    $nw = $gw;
+    while(count($nw->childWave)>0){
+        $nw = $nw->childWave[count($nw->childWave)-1];
+    }
+    
+    while($nw->level > 2){
+        $child = json_decode(json_encode($nw));
+        $child->id = uniqid();
+        $child->pid = $nw->id;
+        $child->level = $nw->level - 1;
+        $nw->childWave[] = $child;
+        $nw = $child;
+    }
+    
+    if(empty($csv)){
+        return false;
+    }else{
+        $arrow = getArrow($gw);
+        $ac = countIndexArrow($gw);
+        $format="INSERT INTO wavedaily (code,dt,gw,arrow,ac) VALUES('%s','%s','%s','%s',%d) ON DUPLICATE KEY UPDATE dt='%s',gw='%s',arrow='%s',ac=%d";
+        $strQuery = sprintf($format,$code,formatDate($csv[0][0]),json_encode($gw),$arrow,$ac,formatDate($csv[0][0]),json_encode($gw),$arrow,$ac);
+        $mysql -> query($strQuery);
     }
 }
