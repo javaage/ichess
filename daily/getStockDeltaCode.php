69fc73e67fbbbad61d9a04d253af9e2dcc38f945
Spring <hb_java@sina.com>
2018-11-11 19:55:25 +0800

    add pb>0

--- daily/getStockDeltaCode.php
+++ daily/getStockDeltaCode.php
@@ -2,7 +2,7 @@
 require '../header.php';
 require '../common.php';
 
-$sql = "select w.code,a.name,a.py,w.min,w.max,w.duration,w.ac,w.arrow,a.ttm,a.pb from (select * from wavestock where time = (select max(time) from wavestock)) w inner join allstock a on substr(w.code,1,6) =substr(a.code,3,6) order by w.duration desc";
+$sql = "select w.code,a.name,a.py,w.min,w.max,w.duration,w.ac,w.arrow,a.ttm,a.pb from (select * from wavestock where time = (select max(time) from wavestock)) w inner join allstock a on substr(w.code,1,6) =substr(a.code,3,6) where a.ttm>0 and a.pb>0 order by w.duration desc";
 $result = $mysql -> query($sql);
 $codes = array();
 while ($result && $mr = $result->fetch_array(MYSQLI_ASSOC)) {
